7f19147aa2c38e2a8865f62263799e8b7485b5b4
Aman2205saini <137211234+Aman2205saini@users.noreply.github.com>
2025-01-20 20:25:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+var para=document.getElementById("para")
+var intial=0;
+function increment(){
+	alert("Current counter value (before incrementing): " + intial);
+	intial+=1;
+  para.textContent=intial
+}
